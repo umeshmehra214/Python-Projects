dde4beadfc7046970e7b4cf33f51c10af104dc0b
Umesh Mehra <126617744+umeshmehra214@users.noreply.github.com>
2023-03-11 23:28:58 +0530

    Update Python_QR_Code.py

--- Python_QR_Code.py
+++ Python_QR_Code.py
@@ -1,21 +1,26 @@
-# first we intall the qrcode in our terminal ---- pip install qrcode
+# There are two ways to Generat QR Code in Python ------>
 
-# 1 way to create simple QR Code--->
+# 1. Simple QR Code
+# 2. Advanced QR Code
 
-# import qrcode
+# First we intall QR module in our system by use this command ----- >   pip install qrcode
 
-# img = qrcode.make("https://www.linkedin.com/in/umeshmehra214/")  
-# img.save("text.png")
+# 1. Generating a Simple QR Code :-----
 
-# 2 Way to create QR Code----->
+import qrcode
+img = qrcode.make("https://www.linkedin.com/in/umeshmehra214/")  
+img.save("Linkedin_profile.png")       # A Linkedin_profile QR code image will be created scan QR Code Image and get results  
+
+
+# 2. Generating an Advanced QR Code : ------
 
 import qrcode
 from PIL import Image
 
 qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=4)
 
-qr.add_data('https://www.sololearn.com/profile/28491225')
+qr.add_data('https://github.com/umeshmehra214')
 qr.make(fit=True)
 
 img=qr.make_image(fill_color="red",back_color="blue")
-img.save("newqrcode.png")
+img.save("Github_profile.png")        # A Github_profile QR code image will be created scan QR Code Image and get results
